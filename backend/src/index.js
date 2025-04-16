2ff44365347a9de2c4b31d2f84cedd673d62c7b7
Mankirat364 <mankirat.techguild@gmail.com>
2025-04-16 21:12:06 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://fullstackchatapp-2-rqo6.onrender.com",
     credentials: true,
   })
 );
